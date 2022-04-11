baade579dc0df92ea560ba861ff192ef083e7892
BuildTools <unconfigured@null.spigotmc.org>
2022-04-11 18:20:35 +0200

    Added (Questionable) yaml parsing and unit test for Capterra

--- Input Readers/CapterraFileReader.cs
+++ Input Readers/CapterraFileReader.cs
@@ -1,14 +1,38 @@
 ﻿using GartnerDevelopmentTest.Model;
-
+using System.Text.RegularExpressions;
+using YamlDotNet;
+using YamlDotNet.Serialization;
 
 namespace GartnerDevelopmentTest.Input_Readers
 {
     public class CapterraFileReader : IFileReader
     {
+        //private static Deserializer Deserializer = (Deserializer)new DeserializerBuilder().Build();
+        //I hate doing manual YAML loading but I don't have time to fight the deserialiser :/
+        private readonly Regex getValueRegex = new Regex("\"(?<value>.+?)\"", RegexOptions.Compiled);
+
+
         List<MySqlRowEntity> IFileReader.Read(string v)
         {
-            throw new NotImplementedException();
+            IEnumerable<string> file = File.ReadAllLines(v);
+            if (file == null || file.Count() == 0)
+                throw new Exception("Failed reading Yaml file");
 
+            List<MySqlRowEntity> result = new List<MySqlRowEntity>();
+            file = file.Skip(1); //header line
+
+
+            for (int i = 0; i < file.Count() / 4; i++)
+            {
+                var lines = file.Skip(i*4).Take(4).ToArray();
+                var tags = getValueRegex.Match(lines[1]).Groups[1].Value;
+                var name = getValueRegex.Match(lines[2]).Groups[1].Value;
+                var twitter = getValueRegex.Match(lines[3]).Groups[1].Value;
+
+                result.Add(new MySqlRowEntity(tags.Split(','), name, twitter));
+            }
+
+            return result;
         }
     }
 }
---
+++ Model/CapterraYamlEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GartnerDevelopmentTest.Model
+{
+    public class CapterraYamlEntity
+    {
+        public YamlRowEntity[] Rows { get; set; }
+
+    }
+
+    public class YamlRowEntity
+    {
+        public string tags { get; set; }
+        public string name { get; set; }
+        public string twitter { get; set; }
+    }
+}
